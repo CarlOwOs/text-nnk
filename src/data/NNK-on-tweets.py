2f2ba6e124ddc81859bb85e6deaf4aac781690b0
Carlos Hurtado Comín <carlos.hurtado.comin@gpie03.tsc.upc.edu>
2023-01-24 22:52:04 +0100

    nnk on tweets number of samples

--- src/data/NNK-on-tweets.py
+++ src/data/NNK-on-tweets.py
@@ -10,7 +10,6 @@ from src.utils.nnk_graph import nnk_graph
 parser = argparse.ArgumentParser()
 parser.add_argument('--output_file', default='./data/test/data.csv')
 parser.add_argument('--model', default='bert-base-cased')
-parser.add_argument('--dataset', default='tweet_eval.sentiment')
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -26,8 +25,8 @@ if __name__ == "__main__":
     dataset = dataset["test"]
     
     # split the dataset by label and sample
-    dataset_negative = dataset.filter(lambda example: example["label"] == 0).shuffle(seed=14).select(range(1000))
-    dataset_neutral = dataset.filter(lambda example: example["label"] == 1).shuffle(seed=14).select(range(1000))
+    dataset_negative = dataset.filter(lambda example: example["label"] == 0).shuffle(seed=14).select(range(600))
+    dataset_neutral = dataset.filter(lambda example: example["label"] == 1).shuffle(seed=14).select(range(600))
     dataset_positive = dataset.filter(lambda example: example["label"] == 2).shuffle(seed=14).select(range(600))
     dataset = concatenate_datasets([dataset_negative, dataset_neutral, dataset_positive])
 
